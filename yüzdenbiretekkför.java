d6e16490488be0bb8677ef052b2f91f7dfa13010
Gamzesrn <117711013+Gamzesrn@users.noreply.github.com>
2022-12-16 14:27:47 +0300

    Add files via upload

---
+++ yüzdenbiretekkför.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Gamze
+ */
+public class yüzdenbiretekkför {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 50; i++) {
+          System.out.println(100-(2*i-1));
+        } 
+        
+        System.out.println(" ");
+        
+       // her iki programda aynı işlemi yapar
+       
+        for (int i = 0; i < 50; i++) {
+            System.out.println(100-(2*i+1));
+        }
+    }
+   
+}
